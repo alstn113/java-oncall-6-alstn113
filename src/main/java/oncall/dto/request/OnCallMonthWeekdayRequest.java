be2efc66a820747ca3566fb82c1d9cb68ad2c90f
김민수 MINSOO KIM <alstn113@gmail.com>
2023-12-16 17:10:53 +0900

    refactor: OnCallMonthWeekdayRequest 매직 넘버 수정

--- src/main/java/oncall/dto/request/OnCallMonthWeekdayRequest.java
+++ src/main/java/oncall/dto/request/OnCallMonthWeekdayRequest.java
@@ -8,12 +8,13 @@ import oncall.view.util.InputUtil;
 
 public record OnCallMonthWeekdayRequest(String input) {
     private static final String DELIMITER = ",";
+    private static final int monthAndWeekdaySize = 2;
 
     public OnCallMonthWeekday toOnCallMonthWeekday() {
         InputUtil.validateInputNotEmpty(input);
 
         List<String> monthAndWeekday = InputUtil.parseToList(input, DELIMITER);
-        if (monthAndWeekday.size() != 2) {
+        if (monthAndWeekday.size() != monthAndWeekdaySize) {
             throw new InvalidInputException(ErrorMessage.INVALID_ONCALL_MONTH_WEEKDAY_FORMAT);
         }
 
